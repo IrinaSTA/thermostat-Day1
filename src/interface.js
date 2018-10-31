1bb7517925022cf95487221e70ad589ae7f921ec
Irina Baldwin <irinabaldwin@Irinas-MacBook-Air.local>
2018-10-31 15:03:53 +0000

    Finished jquery set up (except css for green vs red color)

--- src/interface.js
+++ src/interface.js
@@ -1,19 +1,42 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
-
+  updateEnergyUsage();
 
   $("#up").on('click', function() {
     thermostat.up();
     updateTemperature();
+    updateEnergyUsage();
   });
 
   $("#down").on('click', function() {
     thermostat.down();
     updateTemperature();
+    updateEnergyUsage();
   });
 
-function updateTemperature() {
+  $("#reset").on('click', function() {
+    thermostat.reset();
+    updateTemperature();
+    updateEnergyUsage();
+  });
+
+  $("#pwr_svg_on").on('click', function(){
+    thermostat.turnOnPowerSavingMode();
+    updateEnergyUsage();
+  });
+
+  $("#pwr_svg_off").on('click', function (){
+    thermostat.turnOffPowerSavingMode();
+    updateEnergyUsage();
+  });
+
+  function updateTemperature() {
     $('#temperature').text(thermostat.degrees);
   };
+
+  function updateEnergyUsage() {
+    $('#energy_usage').text(thermostat.energyUsage());
+  };
+
 });

401f525a0ee4c9385ed18732146128fc20bac9bc
Irina Baldwin <irinabaldwin@irinas-air.lan>
2018-10-31 22:29:15 +0000

    Basic API feed complete; city search still to do, also to switch to Celsius from Kalvin

--- src/interface.js
+++ src/interface.js
@@ -21,6 +21,17 @@ $(document).ready(function() {
     updateEnergyUsage();
   });
 
+// Playing around with toggle
+  // $("#pwr_svg").on('click', function(){
+  //   thermostat.turnOffPowerSavingMode();
+  //   updateEnergyUsage();
+  // });
+
+  // $("#pwr_svg_off1").on('click', function (){
+  //   thermostat.turnOffPowerSavingMode();
+  //   updateEnergyUsage();
+  // });
+
   $("#pwr_svg_on").on('click', function(){
     thermostat.turnOnPowerSavingMode();
     updateEnergyUsage();
@@ -41,3 +52,28 @@ $(document).ready(function() {
   };
 
 });
+
+$.ajax({
+  url: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=77380d472b8cd2201e8db5af55c82a7c",
+  // data: {
+  //   id: "77380d472b8cd2201e8db5af55c82a7c"
+  // },
+  type: "GET",
+  dataType: "json",
+})
+
+.done(function(json) {
+  $("<h3>").text(json.main["temp"]).appendTo("body");
+  $("<div class=\"content\">").html(json.html).appendTo("body");
+})
+
+.fail(function( xhr, status, errorThrown ) {
+  alert( "Sorry, there was a problem!" );
+  console.log( "Error: " + errorThrown );
+  console.log( "Status: " + status );
+  console.dir( xhr );
+})
+
+// .always(function( xhr, status ) {
+//   alert( "The request is complete!" );
+// });
